50dcf9a8350331fcacb677a56278e34e6cb7ee01
Sonikku <soniczoneyearbook@gmail.com>
2023-05-05 23:08:43 +0200

    * FEATURES ADDED * - Motorola S Record parser - Able to load program into RAM and run it successfully from file - Fixed bugs

--- main.go
+++ main.go
@@ -20,13 +20,30 @@ type Settings struct {
 
 // Main Variables
 var rambuffer = make([]byte, 255) // Main RAM buffer (1:1) correspondence with HC05 RAM in bootloader mode
-var length byte = 1               // Length indicator sen to the bootloader, the count includes itself hence we set it to 1
+var length byte = 1               // Length indicator sent to the bootloader, the count includes itself hence we set it to 1
 var selector = 0
 
 var rxbuffer = make([]byte, 1024) // Main serial reception buffer
 var rxbuffercount = 0
 var tmpbuf = make([]byte, 100)
 var port serial.Port
+
+const RAM_0050 = 1
+
+// 68HC705C8 memory area images
+//------------------------------
+
+var RAM = make([]byte, 176)      // Main RAM + STACK
+var USER_PROM = make([]byte, 96) // If RAM1 bit = 0
+var RAM2 = make([]byte, 96)      // If RAM1 bit = 1
+var PROM = make([]byte, 7584)
+var OPTION_REGISTER byte            // Address 0x1FDF
+var MASK_OPTION_REGISTER1 byte      // Address 0x1FF0
+var MASK_OPTION_REGISTER2 byte      // Address 0x1FF1
+var PROM_VECTORS = make([]byte, 12) // 1FF2 - 1FFF
+
+var RAM_SIZE_LOADED uint16 = 0
+var RAM_PROGRAM_START uint16 = 0
 
 // Test program to be written and run on the HC05 to confirm the board/CPU/serial connection/power is well
 // This is the object code compiled with CASM05Z and the S19 record converted directly to binary
@@ -57,18 +74,116 @@ var testprog = []byte{
 // Utility Functions
 //-------------------------------------------------------------------------------------------------------------------
 
+func asciihex2bin(digit1 byte, digit2 byte) byte {
+	var returnvalue byte = 0
+
+	if digit1 > 0x2F && digit1 < 0x3A || digit1 > 0x40 && digit1 < 0x47 {
+		// Value is between ASCII '0'..'9' and ASCII 'A'..'F'
+		if digit1 > 0x2F && digit1 < 0x3A {
+			returnvalue = digit1 - 0x30
+		}
+		// Value is between ASCII 'A'.. 'F'
+		if digit1 > 0x40 && digit1 < 0x47 {
+			returnvalue = digit1 - 0x37
+		}
+		returnvalue = returnvalue << 4
+	}
+
+	if digit2 > 0x2F && digit2 < 0x3A || digit2 > 0x40 && digit2 < 0x47 {
+		// Value is between ASCII '0'..'9' and ASCII 'A'..'F'
+		if digit2 > 0x2F && digit2 < 0x3A {
+			returnvalue |= digit2 - 0x30
+		}
+		// Value is between ASCII 'A'.. 'F'
+		if digit2 > 0x40 && digit2 < 0x47 {
+			returnvalue |= digit2 - 0x37
+		}
+
+	}
+	return returnvalue
+}
+
+// LoadSrec --------------------------------------------------------------------------------------------------------
 // Name: LoadSrec
 // Function: Load Motorola S-Record file from the disk and parse it, then decode all the data to binary and store
 //
 //	that in the appropriate buffer
 //
-// Parameters: Full path to the file that shall be opened, Loading options
+// Parameters: Full path to the file that shall be opened, Target Area in MCU, Pointer to variable where length shall be stored
 // Returns: Result of operation
 // ----------------------------------------------------------------------------------------------------------------
-func LoadSrec(pathtofile string, loadoption uint8) int {
+func LoadSrec(path string, targetarea uint8, objectlength *uint16) int {
+
+	var address uint16
+
+	*objectlength = 0
+	srec, err := os.Open(path)
+	if err != nil {
+		fmt.Println(" Error opening file! ")
+	}
+	defer srec.Close()
+	srecords := bufio.NewScanner(srec)
+	for srecords.Scan() {
+		line := srecords.Text()
+		// Each line of the S-record is parsed here
+		if line[0] == 'S' && line[1] == '1' {
+			// Valid S-Record, extract record length
+
+			len := asciihex2bin(line[2], line[3])
+			// The length value includes 3 extra i.e. address and check byte, so we subtract to get total size of bytes
+			len -= 3
+			address = uint16(asciihex2bin(line[4], line[5]))
+			address = address << 8
+			address |= uint16(asciihex2bin(line[6], line[7]))
+
+			// We now have the address and we have the data, see where we must store it
+			if targetarea == RAM_0050 {
+				// Target memory is the MCU RAM, the address supplied must fall in that range
+				if address < 0x0050 && address > 0x00BF {
+					fmt.Println(" Error: S-Record address falls outside of allowable memory range!")
+					return -1
+				}
+				// The very first S-record is usually where the program starts, so we grab that as the start address of the program
+				if RAM_PROGRAM_START == 0 {
+					RAM_PROGRAM_START = address
+				}
+				// Range check is OK, decode s-record to the buffer
+				var offset = 8
+				for n := 0; n < int(len); n++ {
+					RAM[(address-0x50)+uint16(n)] = asciihex2bin(line[offset], line[offset+1])
+					offset += 2
+					*objectlength++
+				}
+			}
+		}
+	}
+
 	return 0
 }
 
+// -------------------------------------------------------------------------------------------------------------------
+// Name: DumpMemory
+// Function: Hex dump to the console, the memory area passed by reference and the size of the passed memory area
+// Parameters: Pointer to buffer, Size of memory area (in bytes)
+// Returns: void
+// -------------------------------------------------------------------------------------------------------------------
+func DumpMemory(buffer []byte, size int, offset uint16) {
+
+	var addr uint16 = 0
+	for {
+		fmt.Printf("%04X:   %02X %02X %02X %02X %02X %02X %02X %02X    %02X %02X %02X %02X %02X %02X %02X %02X |\r\n", addr+offset,
+			buffer[addr+0], buffer[addr+1], buffer[addr+2], buffer[addr+3], buffer[addr+4], buffer[addr+5], buffer[addr+6], buffer[addr+7],
+			buffer[addr+8], buffer[addr+9], buffer[addr+10], buffer[addr+11], buffer[addr+12], buffer[addr+13], buffer[addr+14], buffer[addr+15])
+		addr += 16
+		if addr >= uint16(size) {
+			break
+		}
+	}
+}
+
+// -------------------------------------------------------------------------------------------------------------------
+// Main Function
+// -------------------------------------------------------------------------------------------------------------------
 func main() {
 	fmt.Println("                                              ")
 	fmt.Println("╔════════════════════════════════════════════╗")
@@ -120,10 +235,12 @@ func main() {
 	go SerialRx()
 	fmt.Println("   ** READY TO PROGRAM TARGET MC68HC705C8  **   ")
 	fmt.Println("Available Commands:")
-	fmt.Println(" * TEST - Load test program into HC05 and check response (supports official boards and MIDON PROG05 programmer)")
-	fmt.Println(" * DEMO - Load simple demonstration program into HC05 that toggles PORT A pins (use this to confirm your MCU is OK)")
-	fmt.Println(" * LOAD - Load user application into HC05 RAM and execute (specify a .S19 file")
-	fmt.Println(" * QUIT - Quit this program ")
+	fmt.Println(" * TEST    - Load test program into HC05 and check response (supports official boards and MIDON PROG05 programmer)")
+	fmt.Println(" * DUMP    - Dump buffer by area (A: RAM, B: PROM ($20-$4F)")
+	fmt.Println(" * DEMO    - Load simple demonstration program into HC05 that toggles PORT A pins (use this to confirm your MCU is OK)")
+	fmt.Println(" * LOADRAM - Load user application into HC05 RAM and execute (specify a .S19 file)")
+	fmt.Println(" * LOAD    - Load user application into memory for EPROM programming")
+	fmt.Println(" * QUIT    - Quit this program ")
 
 	//--------------------------------------------------------------------------------------
 	// User Input Handling
@@ -134,6 +251,58 @@ func main() {
 		userinput, _ := reader.ReadString('\n')
 		//fmt.Println(userinput) // ONLY RETAINED FOR DEBUGGING, REMOVE IT
 		// User input obtained, process it
+
+		// DUMP command
+		if strings.Contains(userinput, "DUMP A") {
+			fmt.Println("HEX Dump of RAM buffer ($005F - $00FF in the HC05 memory map)")
+			DumpMemory(RAM, len(RAM), 0x50)
+		}
+
+		// LOADRAM command
+		if strings.Contains(userinput, "LOADRAM") {
+			fmt.Printf(" Enter path and file name of S-record file: ")
+			path, _ := reader.ReadString('\n')
+			path = strings.Trim(path, "\n")
+			path = strings.Trim(path, "\r")
+
+			// Clear buffer prior to loading
+			for n := range RAM {
+				RAM[n] = 0
+			}
+
+			LoadSrec(path, RAM_0050, &RAM_SIZE_LOADED)
+			fmt.Printf("S-Record loaded Successfully. %d bytes written to buffer\r\n", RAM_SIZE_LOADED)
+			fmt.Println("Please enable loader (9.4V on IRQ pin) and reset the MCU, then press ENTER when ready!")
+			anykey, _ := reader.ReadByte()
+			if anykey > 0 {
+				length = byte(RAM_SIZE_LOADED)
+				length++
+				//fmt.Printf("Length Indicator (1st byte) = %d\r\n", length)
+				fmt.Printf("Upload to target")
+				selector = int(RAM_PROGRAM_START - 0x50)
+				var p = make([]byte, 1)
+
+				// Send length to the bootloader
+				p[0] = length
+				_, err := port.Write(p[0:])
+				if err == nil {
+					for n := 0; n < int(length-1); n++ {
+						p[0] = RAM[selector]
+						time.Sleep(5 * time.Millisecond)
+						_, err := port.Write(p[0:])
+
+						if err != nil {
+							fmt.Println("Error writing byte to target.. ")
+						} else {
+							fmt.Printf(".")
+						}
+						selector++
+					}
+					fmt.Println(" DONE!")
+					fmt.Println(" Program Running!")
+				}
+			}
+		}
 
 		// Quit command
 		//--------------
